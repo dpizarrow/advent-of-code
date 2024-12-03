477785ab67061150c57e393228f544788ea4e1c6
dpizarrow <diego.pizarro0@gmail.com>
2024-12-03 19:03:34 -0300

    feat: day 1 part 1 done

---
+++ day-01/part-1.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import sys
+
+
+def main(input_file: str):
+    try:
+        left_list, right_list = [], []
+        file_path = Path(input_file)
+
+        if not file_path.is_file():
+            print(f"Error: the file {input_file} does not exist")
+            return
+
+        with file_path.open("r", encoding="utf-8") as f:
+            for line in f.readlines():
+                l, r = line.split("   ")
+                left_list.append(int(l))
+                right_list.append(int(r))
+
+        left_list = sorted(left_list)
+        right_list = sorted(right_list)
+
+        total_distance = sum([abs(x - y) for x, y in zip(left_list, right_list)])
+        print(f"Total distance is {total_distance}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python part-1.py <path_to_input>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+
+    main(input_file)
